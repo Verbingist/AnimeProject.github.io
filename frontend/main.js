98346c2df363951893210a2983c53c2b2a5a254f
Verbingist <vip.den767@mail.ru>
2025-08-07 17:04:31 +0300

    added more adaptive

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,16 @@
-let obj = {
-  name: 'igor',
-  age: 4,
-};
+// let obj = {
+//   name: 'igor',
+//   age: 4,
+// };
 
-obj = JSON.stringify(obj);
+// obj = JSON.stringify(obj);
 
-fetch('http://localhost:8001/test.php', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: obj,
-})
-.then(result => result.json())
-.then(result => console.log(result))
+// fetch('http://localhost:8001/test.php', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: obj,
+// })
+// .then(result => result.json())
+// .then(result => console.log(result))
